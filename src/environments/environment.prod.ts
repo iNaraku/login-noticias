0c9c08efc16207fcf930b24fd8bc15c3dcd90d7c
iNaraku <isc.carlos.ra@gmail.com>
2021-04-13 20:19:23 -0500

    app prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseApiKey: 'AIzaSyD06DbQThXe8s4tK7GZ0p_ckMVz32hKXcg',
+  firebaseApiUrl: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty',
+
+  noticiasApiKey: '19a697d2085749e3afc001b7b450e170',
+  noticiasApiUlr: 'https://newsapi.org/v2'
 };
